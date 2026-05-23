dc1321282235044db62387273c4bac24f95eeba7
arenterprises-gif <arushrichhariya324@gmail.com>
2026-05-23 15:11:38 +0530

    Delete src directory

--- src/components/PoweredBy.tsx
+++
@@ -1,7 +0,0 @@
-export default function PoweredBy() {
-  return (
-    <div className="powered-by" style={{ padding: '12px 0', marginTop: 8 }}>
-      Powered by <span>Clerkneu Technology and Solutions</span>
-    </div>
-  )
-}
